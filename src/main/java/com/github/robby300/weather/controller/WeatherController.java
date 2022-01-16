e3e2527e1cd04c6a072c2948f5683d365e6628c4
Robert Imangulov <robby300im@gmail.com>
2022-01-16 23:40:53 +0500

    added logic for getting weather

--- src/main/java/com/github/robby300/weather/controller/WeatherController.java
+++ src/main/java/com/github/robby300/weather/controller/WeatherController.java
@@ -1,10 +1,24 @@
 package com.github.robby300.weather.controller;
 
+import com.github.robby300.weather.service.WeatherHistoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/weather")
 public class WeatherController {
+    private final WeatherHistoryService weatherHistoryService;
+    @Autowired
+    public WeatherController(WeatherHistoryService weatherHistoryService) {
+        this.weatherHistoryService = weatherHistoryService;
+    }
 
+    @GetMapping
+    public String getWeather() throws IOException, InterruptedException {
+        return WeatherHistoryService.findInDb();
+    }
 }
--- src/main/java/com/github/robby300/weather/service/WeatherHistoryService.java
+++ src/main/java/com/github/robby300/weather/service/WeatherHistoryService.java
@@ -1,15 +1,60 @@
 package com.github.robby300.weather.service;
 
+import com.github.robby300.weather.model.WeatherHistory;
 import com.github.robby300.weather.repo.WeatherHistoryRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.InetSocketAddress;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Optional;
+
 @Service
 public class WeatherHistoryService {
-    private final WeatherHistoryRepo weatherHistoryRepo;
+    private static WeatherHistoryRepo weatherHistoryRepo;
 
     @Autowired
     public WeatherHistoryService(WeatherHistoryRepo weatherHistoryRepo) {
         this.weatherHistoryRepo = weatherHistoryRepo;
+    }
+
+    public static String findInDb() throws IOException, InterruptedException {
+        Optional<WeatherHistory> weatherToday = weatherHistoryRepo.findById(LocalDate.now());
+        String weather = "";
+        if (weatherToday != null) {
+            weather = weatherToday.get().getWeatherValue();
+        } else {
+            weather = getFromYandex();
+            WeatherHistory weatherHistory = new WeatherHistory(LocalDate.now(), weather);
+            weatherHistoryRepo.save(weatherHistory);
+        }
+        return weather;
+    }
+
+    public static String getFromYandex() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .followRedirects(HttpClient.Redirect.ALWAYS)
+                .connectTimeout(Duration.ofSeconds(4))
+                .build();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(URI.create("https://yandex.ru/"))
+                .build();
+
+        String response = client.send(request, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8)).body();
+
+        return response.substring(response.indexOf("\"weather__temp\">"), response.indexOf("\"weather__temp\">") + 4);
+
     }
 }
