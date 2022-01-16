7febf5319c9c6492f5275daaf96e8c6b28faeb8d
Robert Imangulov <robby300im@gmail.com>
2022-01-16 22:25:13 +0500

    Added @Repository annotation

--- src/main/java/com/github/robby300/weather/repo/WeatherHistoryRepo.java
+++ src/main/java/com/github/robby300/weather/repo/WeatherHistoryRepo.java
@@ -2,8 +2,10 @@ package com.github.robby300.weather.repo;
 
 import com.github.robby300.weather.model.WeatherHistory;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 
+@Repository
 public interface WeatherHistoryRepo extends CrudRepository<WeatherHistory, LocalDate> {
 }
